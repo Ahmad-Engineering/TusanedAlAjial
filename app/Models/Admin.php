bf3aef230e7e562753336f0d660fefce516b38a4
Ahmad Almabhoh <az540546@gmail.com>
2022-02-20 11:33:44 +0200

    Setup-Admin-Settings-Enviroment

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -10,15 +10,23 @@ class Admin extends Authenticatable
 {
     use HasFactory;
 
-    public function social () {
+    public function social()
+    {
         return $this->hasOne(AdminSocial::class, 'admin_id', 'id');
     }
 
-    public function categories () {
+    public function categories()
+    {
         return $this->hasMany(Category::class, 'admin_id', 'id');
     }
 
-    public function posts () {
+    public function posts()
+    {
         return $this->hasMany(Post::class, 'admin_id', 'id');
+    }
+
+    public function setting()
+    {
+        return $this->hasOne(Setting::class, 'admin_id', 'id');
     }
 }
